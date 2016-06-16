0081175351092fa071f9181fd02cb55700afda16
Wadim Grasza <wadim.grasza@gmail.com>
2016-06-16 12:58:26 +0200

    Replace object literal shorthand syntax in bin/index.js
    bin/index.js is not transpiled so it should not use the shorthand syntax

--- bin/index.js
+++ bin/index.js
@@ -14,7 +14,7 @@ const localCli = path.resolve(process.cwd(), 'node_modules/roc/bin/cli');
 if (currentCli !== localCli) {
     // Are we global?
     if (!fileExists(path.resolve(process.cwd(), 'node_modules/.bin/roc'))) {
-        updateNotifier({ pkg }).notify({ defer: false });
+        updateNotifier({ pkg: pkg }).notify({ defer: false });
         require(currentCli);
     } else {
         // Will try to use the local CLI over the global one
